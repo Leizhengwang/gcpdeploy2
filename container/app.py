8034c0de61f06c145eeacce66a0fad77d3203985
Leizhengwang <58499615+Leizhengwang@users.noreply.github.com>
2021-03-28 18:58:32 -0500

    Update app.py

--- container/app.py
+++ container/app.py
@@ -110,5 +110,5 @@ if __name__ == "__main__":
           "******************************************************************"))
     define_model()
     #  app.run() # outside of a Docker container
-    app.run(host='0.0.0.0')  # within a Docker container
+    app.run(host='0.0.0.0',port=8080)  # within a Docker container
 
